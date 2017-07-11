b9e237869993453949a820d6bbbda5971eb3b1e3
Luke Holder <lukemh@gmail.com>
2017-07-11 12:57:45 +0800

    Clarify what the handle is.

--- ogone/Ogone_GatewayAdapter.php
+++ ogone/Ogone_GatewayAdapter.php
@@ -7,6 +7,8 @@ class Ogone_GatewayAdapter extends OffsiteGatewayAdapter
 {
     public function handle()
     {
+        // This is the omnipay class name compatible with `Omnipay::create`.
+        // See https://github.com/thephpleague/omnipay-common/blob/master/src/Omnipay.php#L100-L103
         return "Ogone_Ecommerce";
     }
 }
